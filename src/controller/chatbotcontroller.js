0becf313b8b25565a0df0f4651bfbae3bb894b80
botret123 <96457290+botret123@users.noreply.github.com>
2023-04-01 03:45:22 +0700

    Update chatbotcontroller.js

--- src/controller/chatbotcontroller.js
+++ src/controller/chatbotcontroller.js
@@ -73,10 +73,11 @@ function callSendAPI(sender_psid, response) {
 
   // Send the HTTP request to the Messenger Platform
   request({
-      "uri": "https://graph.facebook.com/v16.0/me/messages",
-      "qs": { "access_token": PAGE_VERIFY_TOKEN },
-      "method": "POST",
-      "json": request_body
+      'uri': 'https://graph.facebook.com/v16.0/me/messages',
+      'qs': { 'access_token': PAGE_VERIFY_TOKEN },
+      'method': "POST",
+      'messaging_type':'RESPONSE'
+      'json': request_body
   }, (err, res, body) => {
       if (!err) {
           console.log('message sent!');
