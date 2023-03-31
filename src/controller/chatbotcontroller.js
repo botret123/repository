b9c4c02273bb0ccd583e9b54ce91fc236eab139f
botret123 <96457290+botret123@users.noreply.github.com>
2023-04-01 04:52:36 +0700

    Update chatbotcontroller.js

--- src/controller/chatbotcontroller.js
+++ src/controller/chatbotcontroller.js
@@ -77,7 +77,7 @@ function callSendAPI(sender_psid, response) {
       "method": "POST",
       "json": request_body
         
-      }
+      
   }, (err, res, body) => {
       if (!err) {
           console.log('----------------------message sent!');
